a366e34eacda1be68e6c78fc13d991ef437fc3f8
shhong1 <shhong1@withtech.co.kr>
2026-08-29 15:33:34 +0900

    Fix: show blank instead of 0 for padding day cells

--- ViewModels/CalendarDayCellViewModel.cs
+++ ViewModels/CalendarDayCellViewModel.cs
@@ -6,5 +6,5 @@ public class CalendarDayCellViewModel
     public bool IsInCurrentMonth { get; init; }
     public string? EmojiStamp { get; set; }
     public bool HasEntry => !string.IsNullOrEmpty(EmojiStamp);
-    public int DayNumber => Date?.Day ?? 0;
+    public string DayNumber => Date?.Day.ToString() ?? string.Empty;
 }
