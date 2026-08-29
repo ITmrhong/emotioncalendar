b6d2e90498be89783a24be74c79866cf15766a36
shhong1 <shhong1@withtech.co.kr>
2026-08-29 14:36:13 +0900

    Initial commit: EmotionCalendarDiary MAUI project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EmotionCalendarDiary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmotionCalendarDiary
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
